264ea1318212f7f0655c550184e79dfbd708dc1e
jusju <jukka.juslin@haaga-helia.fi>
2022-01-27 09:33:02 +0200

    first commit

---
+++ src/main/java/com/example/jukkabookstore/model/Book.java
@@ -0,0 +1,12 @@
+package com.example.jukkabookstore.model;
+
+public class Book {
+	// ATTRIBUTES
+	private String title;
+	private String author;
+	private int year;
+	private String isbn;
+	private double price;
+	
+
+}
